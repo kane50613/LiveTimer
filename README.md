e7da9eff69343dc9d4aa33e8de18bedbf46f9b25
凱恩Kane <50613gary@gmail.com>
2021-12-11 22:34:50 +0800

    add admin page

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # LiveTimer
-LiveTimer for livestreaming
+~~這是一個被學長壓榨的專案~~
+#### **如果有學弟要用請記得案星星**
+
+## 使用教學
+1. 先下載這坨檔案 `Code > Download ZIP`
+2. 先去裝 [`NodeJS`](https://nodejs.org/zh-tw/)
+3. 然後開個終端機切到這個目錄下面打 `npm i`
+4. 然後打 `node server.js`
+
+## 網頁路徑
+- `/`: 輸出頁
+- `/video.html`: 影片頁面
+- `/admin.html`: 管理頁面
+
+## 貢獻
+ - [學長](https://github.com/lifeng-87): 製作了原始專案
+ - [我](https://github.com/Gary50613): 這個專案
--- server.js
+++ server.js
@@ -6,13 +6,17 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 const timers = new Array(4)
+const password = process.env.PASSWORD ||
+	new Array(5).fill(0).map(() => Math.floor(Math.random() * 10)).join('')
 
 class Timer {
+	_started = 0
 	counted = 0
 	interval
 	position = 0
 	
 	constructor(position, title, down) {
+		this._started = down
 		this.counted = down
 		this.position = position
 		this.title = title
@@ -31,50 +35,82 @@ class Timer {
 	
 	reset() {
 		clearInterval(this.interval)
-		this.counted = 0
+		this.interval = null
+		this.counted = this._started ?? 0
 	}
 	
 	stop() {
 		clearInterval(this.interval)
+		this.interval = null
 	}
 	
 	toJSON() {
 		return {
 			position: this.position,
 			title: this.title,
-			counted: this.counted
+			counted: this.counted,
+			paused: this.paused
 		}
+	}
+	
+	get paused() {
+		return !this.interval
 	}
 }
 
 app.use(express.static('web'))
 
 io.on("connection", (socket) => {
+	if(socket.handshake.auth) {
+		if(socket.handshake.auth.toString() !== password)
+			return socket.disconnect()
+		
+		socket._authed = true
+	}
+	
 	socket.emit('init', timers)
 	
 	socket.on("set", ({index, title, down}) => {
+		if(!socket._authed)
+			return
 		if(timers[index])
 			timers[index].stop()
 		timers[index] = new Timer(index, title, down)
-		timers[index].start()
-		socket.emit('init', timers.map(x => x.toJSON()))
+		io.emit('init', timers.map(x => x?.toJSON()))
 	})
 	
 	socket.on("start", (position) => {
+		if(!socket._authed)
+			return
+		if(!timers[position].paused)
+			return
 		timers[position].start()
-		socket.emit('init', timers.map(x => x.toJSON()))
+		socket.emit('init', timers.map(x => x?.toJSON()))
 	})
 	
 	socket.on("stop", (position) => {
+		if(!socket._authed)
+			return
+		if(timers[position].paused)
+			return
 		timers[position].stop()
-		socket.emit('init', timers.map(x => x.toJSON()))
+		io.emit('init', timers.map(x => x?.toJSON()))
 	})
 	
 	socket.on("reset", (position) => {
+		if(!socket._authed)
+			return
 		timers[position].reset()
-		socket.emit('init', timers.map(x => x.toJSON()))
+		io.emit('init', timers.map(x => x?.toJSON()))
+	})
+	
+	socket.on("clear", (position) => {
+		if(!socket._authed)
+			return
+		delete timers[position]
+		io.emit('init', timers.map(x => x?.toJSON()))
 	})
 })
 
 const port = Number(process.env.PORT) || 8080
-server.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}, password is: ${password}`))
